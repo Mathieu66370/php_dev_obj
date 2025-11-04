fecfd38a0a65332a2496826f90f6747f783a9b8f
Mathieu <bosq.mathieu@gmail.com>
2025-11-04 16:54:51 +0100

    maj

---
+++ tableau.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once 'Country.php';
+$countries = [
+    new Country("France", "Paris", 67, "Europe"),
+    new Country("Japon", "Tokyo", 125, "Asie"),
+    new Country("Brésil", "Brasilia", 214, "Amérique du Sud"),
+    new Country("Canada", "Ottawa", 40, "Amérique du Nord"),
+    new Country("Australie", "Canberra", 30, "Océanie")
+];
+foreach ($countries as $country) {
+    echo "<p>" . $country->getInfo() . "</p>";
+}
+
+
